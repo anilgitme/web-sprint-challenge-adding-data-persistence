e647d8162dac9a946140b9ae0267b1c2c9569a65
Anil_Ramcharran <anil.gitme@gmail.com>
2021-04-04 20:50:20 -0400

    finished tasks router working on connecting the server

--- api/task/router.js
+++ api/task/router.js
@@ -1,1 +1,34 @@
 // build your `/api/tasks` router here
+const express = require('express');
+const Task = require('./model')
+const router = express.router();
+
+//get request from task
+router.get('/', async(req, res, next) => {
+    try {
+        const data = await Task.getAll();
+        res.status(201).json(data)
+    } catch (err) {
+        next(err)
+    }
+})
+
+//post task
+
+router.post('/', async(req, res, next) => {
+    try {
+        const newSource = await Task.insert(req.body)
+        res.status(201).json(newSource)
+    } catch (err) {
+        next(err)
+    }
+})
+
+router.use((err, req, res, next) => {
+    res.status(500).json({
+        message: 'something went wrong inside the resource router',
+        errMessage: err.message,
+    })
+})
+
+module.exports = router;
